767658976523f9457c9b269b994f33a8213df660
Richard Yen <richyen3@gmail.com>
2022-09-12 22:11:58 +0000

    add launch script to provision with terraform and deploy with tpaexec in one shot

---
+++ launch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+set -x
+
+if [[ ${1} == '' ]]; then
+  echo "Usage: ${0} <project_dir>"
+  exit 1
+fi
+
+# Where the scripts and config live
+PROJ_DIR="${HOME}/code/edb/bdr-benchmark-kit"
+
+# Where we want to deploy our cluster
+WORK_DIR=${1}
+
+# Create Terraform cluster dir
+python ${PROJ_DIR}/scripts/new-project.py ${WORK_DIR} ${PROJ_DIR}/gold-infrastructure.yml        
+
+# Initialize Terraform cluster
+terraform -chdir=${WORK_DIR} init
+
+# Apply Terraform cluster
+terraform -chdir=${WORK_DIR} apply -var-file=${WORK_DIR}/terraform_vars.json -auto-approve
+
+# Create tpaexec deploy script
+python ${PROJ_DIR}/scripts/pre-deploy.py -a gold ${WORK_DIR} ${PROJ_DIR}/configuration.yml           
+
+# Deploy with tpaexec
+${WORK_DIR}/deploy.sh
